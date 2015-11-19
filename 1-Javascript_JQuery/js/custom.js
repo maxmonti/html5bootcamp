1a061b9c3e7490cfa273668aba2fc3ec17b13f9c
maxmonti <renzomiguelmonti@gmail.com>
2015-11-19 11:43:29 -0300

    AJAX events

--- 1-Javascript_JQuery/js/custom.js
+++ 1-Javascript_JQuery/js/custom.js
@@ -13,7 +13,22 @@ function fadeInSection() {
      
 }
 
-
+$("#ajax-button").click(function(){
+    $.ajax(
+    
+		{url: "http://bootcamp.aws.af.cm/welcome/yourname", success: 
+			function(result){
+				$(" #hidden-section-h1 ").text(result.response);
+				$(" #hidden-section-h1 ").css("color", "inherit");
+								}, 
+		error: function(){
+			$(" #hidden-section-h1 ").css("color", "red");
+			$(" #hidden-section-h1 ").text("Network error");
+						
+			}
+		
+		});
+}); 
 
 $( document ).ready( fadeInSection );
 
