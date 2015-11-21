47ba77c6050d49f398dee8a1b80f52a07958331f
maxmonti <renzomiguelmonti@gmail.com>
2015-11-21 01:44:38 -0300

    some little advances

--- 1-Javascript_JQuery/js/custom.js
+++ 1-Javascript_JQuery/js/custom.js
@@ -13,6 +13,9 @@ function fadeInSection() {
      
 }
 
+
+
+
 $("#ajax-button").click(function(){
     $.ajax(
     
@@ -42,17 +45,28 @@ $("#ajax-button").click(function(){
 		
 		});
 }); 
+
+
+
 $.ajax(
 	{url:"https://api.spotify.com/v1/search?q=rolling+stones&type=album", success:
-		function(result){
-			$("#ajax-content").val(result)
-			$("#sidebar").html('<img src="' + $("#ajax-content").val().albums.items[0].images[0].url + '" />');
+		function(data){
 			
-			}
+			$("#ajax-content").val(data) //Para pruebas
+			
+			alert(data.albums.items.length)
+			$(data.albums.items).each(function(){
+				
+					$("#sidebar").append('<img src="' + data.albums.items[0].images[0].url + '" />');
+				}
+			)
+			
 		}
+	}
 
 
 );
+
 $( document ).ready( fadeInSection );
 
 
