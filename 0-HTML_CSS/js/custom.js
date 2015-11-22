9fdb346ca1af400378e92ee7107c0a217a1a2bbd
Maximiliano <machine@debian>
2015-11-21 23:23:27 -0300

    HTML form

---
+++ 0-HTML_CSS/js/custom.js
@@ -0,0 +1,11 @@
+function clearAll(){
+	
+	$("#inputFirstName").val()
+	$("#inputLastName").val()
+	$("#inputEmail").val()
+	$("#inputBirthday").val()
+	$("#textArea").val()
+	$("#favoriteSport").val("")
+	
+	
+	}
