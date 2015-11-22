1fb54a487c0b9c441cd74b9619e848448117e7a6
Maximiliano <machine@debian>
2015-11-22 17:32:50 -0300

    form

--- 0-HTML_CSS/js/custom.js
+++ 0-HTML_CSS/js/custom.js
@@ -1,11 +1,44 @@
-function clearAll(){
+function validateFields(){
 	
-	$("#inputFirstName").val()
-	$("#inputLastName").val()
-	$("#inputEmail").val()
-	$("#inputBirthday").val()
-	$("#textArea").val()
-	$("#favoriteSport").val("")
+	var regex = /^\w+[.-]?\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	//We blank everything
+	$("#errorFirstName").text("")
+	$("#errorLastName").text("")
+	$("#errorEmail").text("")
+	$("#errorArea").text("")
+	$("#errorSport").text("")
 	
+	
+	if ($("#inputFirstName").val() == ""){
+		$("#errorFirstName").text("Required field")
+		
+		}
+		
+	
+	if ($("#inputLastName").val() == ""){
+		$("#errorLastName").text("Required field")
+		
+		}
+		
+	if ($("#inputEmail").val().match(regex) == null){
+		$("#errorEmail").text("Invalid email")
+		
+		}
+	if ($("#inputBirthday").val() == ""){
+		$("#errorBirthday").text("Required field")
+		
+		}
+	if ($("#textArea").val() == ""){
+		$("#errorArea").text("Required field")
+		}
+		
+	if ($("#favoriteSport").val() == ""){
+		$("#errorSport").text("Required field")
+		}
+	
+	if ($("#textArea").val() == ""){
+		$("#errorArea").text("Required field")
+		}
 	
 	}
+
