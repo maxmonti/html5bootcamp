8823db4fe8084b7318c64959d16ce642632f1411
maxmonti <renzomiguelmonti@gmail.com>
2015-12-01 14:24:50 -0300

    Identation

--- 2-OOP_Inheritance_JS/movies.js
+++ 2-OOP_Inheritance_JS/movies.js
@@ -27,55 +27,43 @@ function Movie(attributes) {
 		this.publish([this.get('title'),"stop"]);
 		}
 	
-	}
+}
 	
 
 function MovieObserver() {
 		
-			this.addSubscriber = function (callback) {
-				this.subscribers[this.subscribers.length] = callback;
-			},
-				
-			this.removeSubscriber = function (callback) {
-				for (var i = 0; i < this.subscribers.length; i++) {
-					if (this.subscribers[i] === callback) {
-						delete(this.subscribers[i]);
-					}
-				}
-			},
-				
-			this.publish = function (what) {
-				for (var i = 0; i < this.subscribers.length; i++) {
-					if (typeof this.subscribers[i] === 'function') {
-						this.subscribers[i](what);
-					}
-				}
-			},
-				
-			this.makePublisher = function (o) { 
-				for (var i in this) {
-					o[i] = this[i];
-					o.subscribers = [];
-				}
+	this.addSubscriber = function (callback) {
+		this.subscribers[this.subscribers.length] = callback;
+	},
+		
+	this.removeSubscriber = function (callback) {
+		for (var i = 0; i < this.subscribers.length; i++) {
+			if (this.subscribers[i] === callback) {
+				delete(this.subscribers[i]);
 			}
-				
-		};
-		
-		
-		
-			
-function PlayBackObserver() {
-	this.watchPlayback = function (movie) {
-		if (movie[1] == "play") {
-			return "Playing " + movie[0] + "...";
-		}else{
-			return "Stopping playback of " + movie[0];
 		}
-
+	},
+				
+	this.publish = function (what) {
+		for (var i = 0; i < this.subscribers.length; i++) {
+			if (typeof this.subscribers[i] === 'function') {
+				this.subscribers[i](what);
+			}
+		}
+	},
+				
+	this.makePublisher = function (o) { 
+		for (var i in this) {
+			o[i] = this[i];
+			o.subscribers = [];
+		}
 	}
+				
 };
-
-function PlayBackConsoleLogger() {
+		
+		
+		
+function PlayBackObserver() {
 	this.logPlayback = function (movie) {
 		if (movie[1] == "play") {
 			console.log("Playing " + movie[0] + "...")
@@ -90,13 +78,11 @@ function PlayBackConsoleLogger() {
 movie1 = new Movie();
 movie1.set('title', 'Enemy');
 play_observer = new PlayBackObserver();
-console_observer = new PlayBackConsoleLogger();
 
 observer = new MovieObserver();
 observer.makePublisher(movie1);
 
 			
-movie1.addSubscriber(play_observer.watchPlayback);
-movie1.addSubscriber(console_observer.logPlayback);
+movie1.addSubscriber(play_observer.logPlayback);
 movie1.play();
 movie1.stop();
