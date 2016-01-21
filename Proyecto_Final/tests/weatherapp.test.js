5edf5def19ade898bbcc586396147584253ee2b0
maxmonti <machine@debian>
2016-01-21 15:29:56 -0300

    Test finished with only one beforeEach statement

--- Proyecto_Final/tests/weatherapp.test.js
+++ Proyecto_Final/tests/weatherapp.test.js
@@ -1,12 +1,12 @@
 
-
+/*
 (function() {
   describe('Testing WidgetsList', function() {
 
-  
-	beforeEach(function() {
 
-	    // set up a default value for your mock
+  	
+	beforeEach(function() {		
+		// set up a default value for your mock
 	    widgetsServiceMock = {
 	        delete: ''
 	    }
@@ -17,16 +17,55 @@
 	        $provide.value('widgetsService', widgetsServiceMock);
 
 	    });
-	    var $controller;
+	    
 	});
   	
 	beforeEach(inject(function(_$controller_){
 		$controller = _$controller_;
 	}));
-
+	
 	
 	describe('Function that returns weather codes', function () {
 		it('should return icon-cloud-wind', function() {
+		      var $scope = {};
+		      var controller = $controller('WidgetsList', { $scope: $scope, widgetsService: widgetsServiceMock });
+		      expect($scope.codeToIcon('0')).toBe('icon-cloud-wind');
+		});
+	});
+  	
+
+
+  });
+}());
+*/
+
+
+(function() {
+  describe('Testing WidgetsList', function() {
+
+	beforeEach(function() {		
+		// set up a default value for your mock
+	    widgetsServiceMock = {
+	        delete: ''
+	    }
+
+	    // use the $provide service to replace ServiceB
+	    // with your mock
+	    module('weatherApp', function($provide) {
+	        $provide.value('widgetsService', widgetsServiceMock);
+
+	    });
+	    
+	});
+  	/*
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+	*/
+	
+	describe('Function that returns weather codes', function () {
+		it('should return icon-cloud-wind', function(_$controller_) {
+		      $controller = _$controller_
 		      var $scope = {};
 		      var controller = $controller('WidgetsList', { $scope: $scope, widgetsService: widgetsServiceMock });
 		      expect($scope.codeToIcon('0')).toBe('icon-cloud-wind');
